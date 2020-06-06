516fc01c8c34c52bf468574dc16d5a6c4f8c43d6
Gabriel Domingues <gabrieldomingues522@gmail.com>
2020-06-06 00:46:44 -0300

    aula 4

---
+++ src/server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const server = express();
+
+//config pasta pública
+server.use(express.static("public"));
+
+//utilizando tmplate engines
+const nunjucks = require("nunjucks");
+nunjucks.configure("src/views", {
+  express: server,
+  noCache: true,
+});
+
+//configurar caminhos
+// home
+// req: requisisção
+//res: resposta
+server.get("/", (req, res) => {
+  return res.render("index.html");
+});
+server.get("/create-point", (req, res) => {
+  return res.render("create-point.html");
+});
+server.get("/search", (req, res) => {
+  return res.render("search-results.html");
+});
+
+//ligar o server
+server.listen(3000);
